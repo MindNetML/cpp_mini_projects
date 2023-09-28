99e90fcc3b888b3314a1c08631f7deb0f631166e
MindNetML <jmjamb81@ymail.com>
2023-09-28 09:56:07 -0700

    updated .gitignore

--- 1printNameAge/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/projects/cpp
-# Build directory: /home/user/projects/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 4maxlimit/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/projects/cpp/4maxlimit
-# Build directory: /home/user/projects/cpp/4maxlimit/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
