a09e980dc6b9ddfd61c960c418cbfbd9e722a924
MindNetML <jmjamb81@ymail.com>
2023-10-07 13:12:45 -0700

    updated to have an io.cpp where functions live

---
+++ 19project/io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// function to get and return a single number from the user
+int readNumber(){
+    int num{};
+    std::cout << "Enter a number to add: ";
+    std::cin >> num;
+    return num;
+}
+
+// Takes a single parameter and outputs the answer
+void writeAnswer(int num){
+    std::cout << "The answer is: " << num << std::endl;
+
+}
