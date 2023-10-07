acccbb57ec50851264be278eda4bfd9e31c1b6ae
MindNetML <jmjamb81@ymail.com>
2023-10-07 13:15:14 -0700

    created a header file for the forward declarations

---
+++ 19project/io.h
@@ -0,0 +1,4 @@
+// Header file for forward declarations
+
+int readNumber(); // forward declare readNumber from io.cpp
+void writeAnswer(int num); // forward declare writeAnswer from io.cpp
--- 19project/main.cpp
+++ 19project/main.cpp
@@ -1,10 +1,8 @@
+#include "io.h"
 // program that reads two different inegers from the user and adds them and outputs the answer
 
-int readNumber(); // forward declare readNumber from io.cpp
-void writeAnswer(int num); // forward declare writeAnswer from io.cpp
 
-
-// variables x and y take the readNumber function. The output, which is a single value for each, gets added and printed by the writeAnswer funcition
+// variables x and y take the readNumber function. The output, which is a single value for each, gets added and printed by the writeAnswer function
 int main(){
     int x{readNumber()};
     int y{readNumber()};
